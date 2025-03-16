4a316a94946824d03d98f0ac4bd59f7ba040bb93
Tsuwagi <viktoria16kutas5@gmail.com>
2025-03-16 23:41:37 +0200

    lab end

--- lab 16 (2).cpp
+++ lab 16 (2).cpp
@@ -2,26 +2,23 @@
 #include <vector>
 using namespace std;
 
-bool isSorted(const vector<int> arr) {
-    for (int i = 0; i < arr.size() - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
+bool isSorted(const vector<int> A) {
+    for (int i = 1; i < 5; i++)
+    if (A[i-1] > A[i]) {
+        return false;
     }
-
     return true;
 }
 
 
 int main() {
     
-    vector<int> arr = {1, 2, 3, 4, 5}; 
+    vector<int> A = {1, 2, 3, 4, 5}; 
 
-    if (isSorted(arr)) {
+    if (isSorted(A)) {
         cout << "Array is sorted" << endl;
     } else {
         cout << "Array is not sorted" << endl;
     }
 
-    return 0;
-}
+    return 0;}
--- lab 16 (3).cpp
+++ lab 16 (3).cpp
@@ -2,67 +2,56 @@
 using namespace std;
 
 int main() {
-    // Создаю массив из 12 чисел
-    const int size = 12;
-    int array[size]; 
     
-    // Прошу пользователя ввести числа
-    cout << "Enter " << size << " elements (only 0, 1 or 2): ";
+    int array[12]; 
     
-    // Считываю числа в массив
-    for (int i = 0; i < size; i++) {
+    cout << "Enter " << 12 << " elements (only 0, 1 or 2): ";
+    
+    for (int i = 0; i < 12; i++) {
         cin >> array[i];
     }
     
-    // Вывожу исходный массив
     cout << "Original array: ";
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < 12; i++) {
         cout << array[i] << " ";
     }
     cout << endl;
     
-    // Подсчитываю, сколько у нас 0, 1 и 2
-    int count0 = 0;  // Счетчик для нулей
-    int count1 = 0;  // Счетчик для единиц
-    int count2 = 0;  // Счетчик для двоек
+    int count0 = 0;  
+    int count1 = 0;  
+    int count2 = 0;  
 
-    // Прохожу по массиву и считаю элементы
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < 12; i++) {
         if (array[i] == 0) {
-            count0++;  // Нашел 0
+            count0++;  
         } 
         else if (array[i] == 1) {
-            count1++;  // Нашел 1
+            count1++;  
         } 
         else if (array[i] == 2) {
-            count2++;  // Нашел 2
+            count2++;  
         }
     }
     
-    // Теперь заполняю массив в порядке: сначала 1, потом 0, потом 2
-    int index = 0;  // С какой позиции начинаем заполнять
+    int index = 0; 
 
-    // Сначала ставлю все единицы
     for (int i = 0; i < count1; i++) {
         array[index] = 1;
-        index = index + 1;
+        index++;
     }
 
-    // Потом ставлю все нули
     for (int i = 0; i < count0; i++) {
         array[index] = 0;
-        index = index + 1;
+        index++;
     }
 
-    // В конце ставлю все двойки
     for (int i = 0; i < count2; i++) {
         array[index] = 2;
-        index = index + 1;
+        index++;
     }
 
-    // Вывожу получившийся массив
     cout << "Rearranged array: ";
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < 12; i++) {
         cout << array[i] << " ";
     }
     
